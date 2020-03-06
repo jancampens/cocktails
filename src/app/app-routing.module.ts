36216b0be0ce49e83148c5f19225a617f35964df
Jan Campens <jan.campens@gmail.com>
2020-03-06 12:47:20 +0100

    Add route for cocktail detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CocktailDetailComponent } from './components/cocktail-detail/cocktail-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'detail/:id', component: CocktailDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
