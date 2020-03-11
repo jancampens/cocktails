117a9692c4629585d309720154dd374ad3bcaaeb
Jan Campens <jan.campens@gmail.com>
2020-03-11 15:12:02 +0100

    Fix ingredient interface

--- src/app/interfaces/ingredient.ts
+++ src/app/interfaces/ingredient.ts
@@ -1,6 +1,6 @@
 export interface Ingredient {
     idIngredient: string;
-    strIngredient: string;
+    strIngredient1: string;
     strDescription: string;
     strType: string;
     strAlcohol: string;
